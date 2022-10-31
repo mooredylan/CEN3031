7d15a8a6e880295266f7e2d00d1f6aaee159fe8a
mooredylan <moore.dylan@ufl.edu>
2022-10-30 20:50:39 -0400

    Basic Main Menu Function

--- Budgetting/LogInForm.cs
+++ Budgetting/LogInForm.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Budgetting
 {
     public partial class LogInForm : Form
     {
+        Thread th;
         private string username, password;
 
         public LogInForm()
@@ -31,7 +33,20 @@ namespace Budgetting
 
         private void logInButton_Click(object sender, EventArgs e)
         {
-            MessageBox.Show($"Username: {username} \nPassword: {password}");
+            Console.WriteLine($"Username: {username} \nPassword: {password}");
+            /*
+             * INSERT USERNAME/PASSWORD HANDLING HERE
+             * CATCH PROFILE OBJECT
+             */
+            this.Close();
+            th = new Thread(showMainMenu);
+            th.SetApartmentState(ApartmentState.STA);
+            th.Start();
+        }
+
+        private void showMainMenu(object obj)
+        {
+            Application.Run(new MainMenu()); //Later, pass profile object as arg for MainMenu()
         }
     }
 }
--- Budgetting/MainMenu.cs
+++ Budgetting/MainMenu.cs
@@ -5,14 +5,37 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Budgetting
 {
     public partial class MainMenu : Form
     {
+        Thread th;
         public MainMenu()
         {
             InitializeComponent();
+            listBox.Items.Add("Recent Budget 1");
+            listBox.Items.Add("Recent Budget 2");
         }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void logOutButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            th = new Thread(showLogIn);
+            th.SetApartmentState(ApartmentState.STA);
+            th.Start();
+        }
+
+        private void showLogIn(object obj)
+        {
+            Application.Run(new LogInForm());
+        }
+
     }
 }
