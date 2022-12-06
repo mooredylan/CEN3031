f2524dfa82300c335491d51b702af36a2b409bb9
CodySchrank <cody.schrank@gmail.com>
2022-12-05 20:44:44 -0800

    changed to net6, added pie charts

--- Budgetting/MainMenu.Designer.cs
+++ Budgetting/MainMenu.Designer.cs
@@ -33,16 +33,28 @@ namespace Budgetting
             this.newBudgetButton = new System.Windows.Forms.Button();
             this.budgetsList = new System.Windows.Forms.ListView();
             this.selectBudgetLabel = new System.Windows.Forms.Label();
-            this.currentBudgetPanel = new System.Windows.Forms.Panel();
-            this.remainingBudgetLabel = new System.Windows.Forms.Label();
-            this.budgetUtilizationLabel = new System.Windows.Forms.Label();
             this.monthlyExpensesLabel = new System.Windows.Forms.Label();
-            this.totalNetIncomeLabel = new System.Windows.Forms.Label();
-            this.taxBracketLabel = new System.Windows.Forms.Label();
-            this.yearlyTotalGrossIncomeLabel = new System.Windows.Forms.Label();
             this.netIncomeLabel = new System.Windows.Forms.Label();
+            this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.totalNetIncomeLabel = new System.Windows.Forms.Label();
+            this.yearlyTotalGrossIncomeLabel = new System.Windows.Forms.Label();
             this.editBudgetButton = new System.Windows.Forms.Button();
-            this.currentBudgetPanel.SuspendLayout();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.formsPlot2 = new ScottPlot.FormsPlot();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.formsPlot3 = new ScottPlot.FormsPlot();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.formsPlot4 = new ScottPlot.FormsPlot();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.formsPlot5 = new ScottPlot.FormsPlot();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.tabPage5.SuspendLayout();
             this.SuspendLayout();
             // 
             // logOutButton
@@ -71,7 +83,6 @@ namespace Budgetting
             // 
             // budgetsList
             // 
-            this.budgetsList.HideSelection = false;
             this.budgetsList.Location = new System.Drawing.Point(90, 509);
             this.budgetsList.Name = "budgetsList";
             this.budgetsList.Size = new System.Drawing.Size(371, 297);
@@ -91,90 +102,53 @@ namespace Budgetting
             this.selectBudgetLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.selectBudgetLabel.Visible = false;
             // 
-            // currentBudgetPanel
-            // 
-            this.currentBudgetPanel.Controls.Add(this.remainingBudgetLabel);
-            this.currentBudgetPanel.Controls.Add(this.budgetUtilizationLabel);
-            this.currentBudgetPanel.Controls.Add(this.monthlyExpensesLabel);
-            this.currentBudgetPanel.Controls.Add(this.totalNetIncomeLabel);
-            this.currentBudgetPanel.Controls.Add(this.taxBracketLabel);
-            this.currentBudgetPanel.Controls.Add(this.yearlyTotalGrossIncomeLabel);
-            this.currentBudgetPanel.Controls.Add(this.netIncomeLabel);
-            this.currentBudgetPanel.Location = new System.Drawing.Point(630, 119);
-            this.currentBudgetPanel.Name = "currentBudgetPanel";
-            this.currentBudgetPanel.Size = new System.Drawing.Size(763, 687);
-            this.currentBudgetPanel.TabIndex = 4;
-            this.currentBudgetPanel.Visible = false;
-            // 
-            // remainingBudgetLabel
-            // 
-            this.remainingBudgetLabel.AutoSize = true;
-            this.remainingBudgetLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.remainingBudgetLabel.Location = new System.Drawing.Point(33, 499);
-            this.remainingBudgetLabel.Name = "remainingBudgetLabel";
-            this.remainingBudgetLabel.Size = new System.Drawing.Size(288, 45);
-            this.remainingBudgetLabel.TabIndex = 6;
-            this.remainingBudgetLabel.Text = "Remaining Budget:";
-            // 
-            // budgetUtilizationLabel
-            // 
-            this.budgetUtilizationLabel.AutoSize = true;
-            this.budgetUtilizationLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.budgetUtilizationLabel.Location = new System.Drawing.Point(33, 581);
-            this.budgetUtilizationLabel.Name = "budgetUtilizationLabel";
-            this.budgetUtilizationLabel.Size = new System.Drawing.Size(281, 45);
-            this.budgetUtilizationLabel.TabIndex = 5;
-            this.budgetUtilizationLabel.Text = "Budget Utilization:";
-            // 
             // monthlyExpensesLabel
             // 
             this.monthlyExpensesLabel.AutoSize = true;
             this.monthlyExpensesLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.monthlyExpensesLabel.Location = new System.Drawing.Point(33, 412);
+            this.monthlyExpensesLabel.Location = new System.Drawing.Point(143, 155);
             this.monthlyExpensesLabel.Name = "monthlyExpensesLabel";
             this.monthlyExpensesLabel.Size = new System.Drawing.Size(284, 45);
             this.monthlyExpensesLabel.TabIndex = 4;
             this.monthlyExpensesLabel.Text = "Monthly Expenses:";
             // 
+            // netIncomeLabel
+            // 
+            this.netIncomeLabel.AutoSize = true;
+            this.netIncomeLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.netIncomeLabel.Location = new System.Drawing.Point(143, 65);
+            this.netIncomeLabel.Name = "netIncomeLabel";
+            this.netIncomeLabel.Size = new System.Drawing.Size(321, 45);
+            this.netIncomeLabel.TabIndex = 0;
+            this.netIncomeLabel.Text = "Monthly Net Income:";
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.Location = new System.Drawing.Point(258, 250);
+            this.formsPlot1.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(374, 311);
+            this.formsPlot1.TabIndex = 7;
+            // 
             // totalNetIncomeLabel
             // 
             this.totalNetIncomeLabel.AutoSize = true;
             this.totalNetIncomeLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.totalNetIncomeLabel.Location = new System.Drawing.Point(33, 246);
+            this.totalNetIncomeLabel.Location = new System.Drawing.Point(143, 155);
             this.totalNetIncomeLabel.Name = "totalNetIncomeLabel";
             this.totalNetIncomeLabel.Size = new System.Drawing.Size(271, 45);
             this.totalNetIncomeLabel.TabIndex = 3;
             this.totalNetIncomeLabel.Text = "Total Net Income:";
             // 
-            // taxBracketLabel
-            // 
-            this.taxBracketLabel.AutoSize = true;
-            this.taxBracketLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.taxBracketLabel.Location = new System.Drawing.Point(33, 159);
-            this.taxBracketLabel.Name = "taxBracketLabel";
-            this.taxBracketLabel.Size = new System.Drawing.Size(184, 45);
-            this.taxBracketLabel.TabIndex = 2;
-            this.taxBracketLabel.Text = "Tax Bracket:";
-            // 
             // yearlyTotalGrossIncomeLabel
             // 
             this.yearlyTotalGrossIncomeLabel.AutoSize = true;
             this.yearlyTotalGrossIncomeLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.yearlyTotalGrossIncomeLabel.Location = new System.Drawing.Point(33, 73);
+            this.yearlyTotalGrossIncomeLabel.Location = new System.Drawing.Point(143, 65);
             this.yearlyTotalGrossIncomeLabel.Name = "yearlyTotalGrossIncomeLabel";
             this.yearlyTotalGrossIncomeLabel.Size = new System.Drawing.Size(299, 45);
             this.yearlyTotalGrossIncomeLabel.TabIndex = 1;
             this.yearlyTotalGrossIncomeLabel.Text = "Total Gross Income:";
-            // 
-            // netIncomeLabel
-            // 
-            this.netIncomeLabel.AutoSize = true;
-            this.netIncomeLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.netIncomeLabel.Location = new System.Drawing.Point(33, 330);
-            this.netIncomeLabel.Name = "netIncomeLabel";
-            this.netIncomeLabel.Size = new System.Drawing.Size(321, 45);
-            this.netIncomeLabel.TabIndex = 0;
-            this.netIncomeLabel.Text = "Monthly Net Income:";
             // 
             // editBudgetButton
             // 
@@ -189,13 +163,117 @@ namespace Budgetting
             this.editBudgetButton.Visible = false;
             this.editBudgetButton.Click += new System.EventHandler(this.editBudgetButton_Click);
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.ItemSize = new System.Drawing.Size(174, 38);
+            this.tabControl1.Location = new System.Drawing.Point(566, 119);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.ShowToolTips = true;
+            this.tabControl1.Size = new System.Drawing.Size(875, 687);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.formsPlot1);
+            this.tabPage1.Controls.Add(this.yearlyTotalGrossIncomeLabel);
+            this.tabPage1.Controls.Add(this.totalNetIncomeLabel);
+            this.tabPage1.Location = new System.Drawing.Point(8, 46);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(859, 633);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Tax";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.formsPlot2);
+            this.tabPage2.Controls.Add(this.netIncomeLabel);
+            this.tabPage2.Controls.Add(this.monthlyExpensesLabel);
+            this.tabPage2.Location = new System.Drawing.Point(8, 46);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(859, 633);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Budget";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // formsPlot2
+            // 
+            this.formsPlot2.Location = new System.Drawing.Point(258, 250);
+            this.formsPlot2.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.formsPlot2.Name = "formsPlot2";
+            this.formsPlot2.Size = new System.Drawing.Size(374, 311);
+            this.formsPlot2.TabIndex = 8;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.formsPlot3);
+            this.tabPage3.Location = new System.Drawing.Point(8, 46);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(859, 633);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Income";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // formsPlot3
+            // 
+            this.formsPlot3.Location = new System.Drawing.Point(122, 47);
+            this.formsPlot3.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.formsPlot3.Name = "formsPlot3";
+            this.formsPlot3.Size = new System.Drawing.Size(623, 534);
+            this.formsPlot3.TabIndex = 8;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.formsPlot4);
+            this.tabPage4.Location = new System.Drawing.Point(8, 46);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(859, 633);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Expenses";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // formsPlot4
+            // 
+            this.formsPlot4.Location = new System.Drawing.Point(118, 49);
+            this.formsPlot4.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.formsPlot4.Name = "formsPlot4";
+            this.formsPlot4.Size = new System.Drawing.Size(623, 534);
+            this.formsPlot4.TabIndex = 9;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.formsPlot5);
+            this.tabPage5.Location = new System.Drawing.Point(8, 46);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(859, 633);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Retirement";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // formsPlot5
+            // 
+            this.formsPlot5.Location = new System.Drawing.Point(97, 153);
+            this.formsPlot5.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.formsPlot5.Name = "formsPlot5";
+            this.formsPlot5.Size = new System.Drawing.Size(668, 431);
+            this.formsPlot5.TabIndex = 9;
+            // 
             // MainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1486, 960);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.editBudgetButton);
-            this.Controls.Add(this.currentBudgetPanel);
             this.Controls.Add(this.selectBudgetLabel);
             this.Controls.Add(this.budgetsList);
             this.Controls.Add(this.logOutButton);
@@ -203,8 +281,14 @@ namespace Budgetting
             this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "MainMenu";
             this.Text = "MainMenu";
-            this.currentBudgetPanel.ResumeLayout(false);
-            this.currentBudgetPanel.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage5.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -215,14 +299,21 @@ namespace Budgetting
         private System.Windows.Forms.Button newBudgetButton;
         private System.Windows.Forms.ListView budgetsList;
         private System.Windows.Forms.Label selectBudgetLabel;
-        private System.Windows.Forms.Panel currentBudgetPanel;
         private System.Windows.Forms.Label netIncomeLabel;
-        private System.Windows.Forms.Label budgetUtilizationLabel;
         private System.Windows.Forms.Label monthlyExpensesLabel;
         private System.Windows.Forms.Label totalNetIncomeLabel;
-        private System.Windows.Forms.Label taxBracketLabel;
         private System.Windows.Forms.Label yearlyTotalGrossIncomeLabel;
-        private System.Windows.Forms.Label remainingBudgetLabel;
         private System.Windows.Forms.Button editBudgetButton;
+        private ScottPlot.FormsPlot formsPlot1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private ScottPlot.FormsPlot formsPlot2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private ScottPlot.FormsPlot formsPlot3;
+        private ScottPlot.FormsPlot formsPlot4;
+        private ScottPlot.FormsPlot formsPlot5;
     }
 }
--- Budgetting/MainMenu.cs
+++ Budgetting/MainMenu.cs
@@ -8,6 +8,7 @@ using System.Windows.Forms;
 using System.Threading;
 using Budgetting.Data;
 using Budgetting.Models;
+using System.Linq;
 
 namespace Budgetting
 {
@@ -37,7 +38,7 @@ namespace Budgetting
             }
             
             this.selectBudgetLabel.Visible = true;
-            this.currentBudgetPanel.Visible = false;
+            this.tabControl1.Visible = false;
             this.editBudgetButton.Visible = false;
         }
 
@@ -75,7 +76,7 @@ namespace Budgetting
         private void budgetsList_SelectedIndexChanged(object sender, EventArgs e)
         {
             this.selectBudgetLabel.Visible = false;
-            this.currentBudgetPanel.Visible = true;
+            this.tabControl1.Visible = true;
 
             if (this.budgetsList.FocusedItem == null) return;
 
@@ -86,17 +87,130 @@ namespace Budgetting
             this.editBudgetButton.Visible = true;
 
             this.updateBudgetLabels();
+            this.updateCharts();
         }
 
         private void updateBudgetLabels()
         {
             this.yearlyTotalGrossIncomeLabel.Text = String.Format("Total Gross Income: {0:C}",this.curBudget.YearlyTotalGrossIncome);
-            this.taxBracketLabel.Text = String.Format("Tax Bracket: {0:P2}",this.curBudget.TaxBracket);
             this.totalNetIncomeLabel.Text = String.Format("Total Net Income: {0:C}",this.curBudget.YearlyTotalNetIncome);
             this.netIncomeLabel.Text = String.Format("Monthly Net Income: {0:C}",this.curBudget.MonthlyTotalNetIncome);
             this.monthlyExpensesLabel.Text = String.Format("Monthly Expenses: {0:C}",this.curBudget.MonthlyTotalExpenses);
-            this.remainingBudgetLabel.Text = String.Format("Remaining Budget: {0:C}",this.curBudget.ExtraBudget);
-            this.budgetUtilizationLabel.Text = String.Format("Budget Utilization: {0:P2}",this.curBudget.BudgetUtilization);
+        }
+
+        private void updateCharts()
+        {
+            this.updatetab1Chart();
+            this.updatetab2Chart();
+            this.updatetab3Chart();
+            this.updatetab4Chart();
+            this.updatetab5Chart();
+        }
+        
+        private void updatetab1Chart()
+        {
+            List<double> values = new List<double>();
+            List<string> names = new List<string>();
+
+            this.addIfExists(this.curBudget.YearlyTotalGrossIncome - this.curBudget.YearlyTotalNetIncome, "Income Tax", values, names);
+            this.addIfExists(this.curBudget.YearlyTotalNetIncome, "Net Income", values, names);
+
+            string centerText = $"Tax\n{values[0] / values.Sum() * 100:00.0}%";
+
+            Color color1 = Color.FromArgb(255, 0, 150, 200);
+
+            var pie = this.formsPlot1.Plot.AddPie(values.ToArray());
+            pie.DonutSize = .6;
+            pie.DonutLabel = centerText;
+            pie.CenterFont.Color = color1;
+            pie.CenterFont.Size = 24;
+            pie.OutlineSize = 2;
+            pie.SliceLabels = names.ToArray();
+
+            this.formsPlot1.Plot.Legend();
+            formsPlot1.Refresh();
+        }
+
+        private void updatetab2Chart()
+        {
+            List<double> values = new List<double>();
+            List<string> names = new List<string>();
+
+            this.addIfExists(this.curBudget.MonthlyTotalExpenses, "Monthly Expenses", values, names);
+            this.addIfExists(this.curBudget.ExtraBudget, "Remaining Income", values, names);
+
+            string centerText = $"Utilization\n{values[0] / values.Sum() * 100:00.0}%";
+
+            Color color1 = Color.FromArgb(255, 0, 150, 200);
+
+            var pie = this.formsPlot2.Plot.AddPie(values.ToArray());
+            pie.DonutSize = .6;
+            pie.DonutLabel = centerText;
+            pie.CenterFont.Color = color1;
+            pie.CenterFont.Size = 24;
+            pie.OutlineSize = 2;
+            pie.SliceLabels = names.ToArray();
+
+            this.formsPlot2.Plot.Legend();
+            formsPlot2.Refresh();
+        }
+
+        private void updatetab3Chart()
+        {
+            List<double> values = new List<double>();
+            List<string> names = new List<string>();
+
+            this.addIfExists(this.curBudget.YearlyGrossIncome, "Gross Income", values, names);
+            this.addIfExists(this.curBudget.YearlyOtherIncome, "Other Income", values, names);
+            this.addIfExists(this.curBudget.YearlyInterestAndDividendIncome, "Interest and Dividends", values, names);
+
+            var pie = this.formsPlot3.Plot.AddPie(values.ToArray());
+            pie.SliceLabels = names.ToArray();
+            pie.ShowPercentages = true;
+
+            this.formsPlot3.Plot.Legend();
+            formsPlot3.Refresh();
+        }
+
+        private void updatetab4Chart()
+        {
+            List<double> values = new List<double>();
+            List<string> names = new List<string>();
+
+            this.addIfExists(this.curBudget.MonthlyInvesting, "Investing", values, names);
+            this.addIfExists(this.curBudget.MonthlySaving, "Savings", values, names);
+            this.addIfExists(this.curBudget.MonthlyRentAndUtilities, "Rent and Utilities", values, names);
+            this.addIfExists(this.curBudget.MonthlyCreditCard, "Credit Card", values, names);
+            this.addIfExists(this.curBudget.MonthlyRecreation, "Recreation", values, names);
+            this.addIfExists(this.curBudget.MonthlyHealthInsurance, "Health Insurance", values, names);
+            this.addIfExists(this.curBudget.MonthlyStudentLoan, "Student Loan", values, names);
+            this.addIfExists(this.curBudget.MonthlyCarPayment, "Car Payment", values, names);
+            this.addIfExists(this.curBudget.MonthlyCarInsurance, "Car Insurance", values, names);
+            this.addIfExists(this.curBudget.MonthlyFood, "Food", values, names);
+
+            var pie = this.formsPlot4.Plot.AddPie(values.ToArray());
+            pie.SliceLabels = names.ToArray();
+            pie.ShowPercentages = true;
+
+            this.formsPlot4.Plot.Legend();
+            formsPlot4.Refresh();
+        }
+
+        private void updatetab5Chart()
+        {
+            List<double> values = new List<double>();
+            List<string> names = new List<string>();
+
+            this.addIfExists(this.curBudget.YearlyGrossIncome, "Gross Income", values, names);
+            this.addIfExists(this.curBudget.YearlyOtherIncome, "Other Income", values, names);
+            this.addIfExists(this.curBudget.YearlyInterestAndDividendIncome, "Interest and Dividends", values, names);
+
+            var pie = this.formsPlot5.Plot.AddPie(values.ToArray());
+            pie.SliceLabels = names.ToArray();
+            pie.ShowPercentages = true;
+
+            this.formsPlot5.Plot.Legend();
+            formsPlot5.Refresh();
         }
 
         private void editBudgetButton_Click(object sender, EventArgs e)
@@ -105,6 +219,15 @@ namespace Budgetting
             th = new Thread(editBudget);
             th.SetApartmentState(ApartmentState.STA);
             th.Start();
+        }
+
+        private void addIfExists(double value, string name, List<double> values, List<string> names)
+        {
+            if(value != 0 && !String.IsNullOrEmpty(name))
+            {
+                values.Add(value);
+                names.Add(name);
+            }
         }
     }
 }
